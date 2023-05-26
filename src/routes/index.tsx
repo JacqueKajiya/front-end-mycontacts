3e900b5adef35d71a327d2a5375da469c1ba78eb
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-05-26 20:35:58 -0300

    feat: added protectedRoutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import { Login } from '../pages/Login'
 import { DashBoard } from '../pages/Dashboard'
+import { ProtectedRoutes } from './protectedRoutes'
 
 export const RoutesMain = () =>{
     return(
         <Routes>
             <Route path="/" element={<Login/>} />
-            <Route path="/dashboard" element={<DashBoard/>} />
+            <Route element={<ProtectedRoutes />}>
+               <Route path="/dashboard" element={<DashBoard/>} />
+            </Route>
         </Routes>
     )
 }
