2e5e548d1286e285e743affc84baf375d0b50e31
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-05-26 20:32:03 -0300

    feat: created Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react"
+import { api } from "../../services/api"
+
+interface Contact{
+    id: string,
+    name: string,
+    email: string,
+    phone: string
+}
+
 export const DashBoard = () =>{
+    const [contacts, setContacts] = useState<Contact[]>([])
+
+    useEffect(() =>{
+        (async () => {
+            const response = await api.get<Contact[]>('contacts')
+
+            setContacts(response.data)
+        })()
+    }, [])
+
     return(
-        <h1>Dashboard</h1>
+        <>
+        <header><button>New Contact</button></header>
+        <main>
+            <ul>
+                {
+                    contacts.map(contact => <li key={contact.id}>{contact.name}</li>)
+                }
+            </ul>
+        </main>
+        </>
     )
 }
