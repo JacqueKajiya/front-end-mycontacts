1d6f71a5c6d754b1abd657a1435160c6fa760052
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-05-31 10:32:16 -0300

    feat: created contacts context

---
+++ src/contexts/ContactsContext.tsx
@@ -0,0 +1,57 @@
+import { ReactNode, createContext, useState } from "react";
+import { IContactData } from "../interfaces/interfaces";
+import { createContactService, deleteContactService, getContactService, updateContactService } from "../services/api.services";
+
+interface IContactsContext {
+    createContact: (data: IContactData) => void;
+    getContact: () => void;
+    updateContact: (data: IContactData, id: string) => void;
+    deleteContact: (id: string) => void;
+    contacts: IContactData[]
+}
+
+interface IContactsProps{
+    children: ReactNode
+}
+
+export const ContactsContext = createContext<IContactsContext>(
+    {} as IContactsContext
+);
+
+const ContactsProvider = ({children}: IContactsProps) =>{
+    const [contacts, setContacts] = useState<IContactData[]>([])
+
+    const createContact = async (data: IContactData) =>{
+        try{
+            await createContactService(data)
+
+            await getContactService()
+        } catch (error) {console.error(error)}
+    }
+
+    const getContact = async() => {
+        try{
+            const getAllContacts = await getContactService()
+
+            setContacts(getAllContacts)
+        } catch (error) {console.error(error)}
+    }
+
+    const updateContact = async (data: IContactData, id: string) => {
+        try{
+            await updateContactService(data, id)
+
+            await getContactService()
+        }catch(error) {console.error(error)}
+    }
+
+    const deleteContact = async (id: string) => {
+        await deleteContactService(id)
+    }
+
+    return(
+        <ContactsContext.Provider value={{createContact, getContact, updateContact, deleteContact, contacts}}>
+            {children}
+        </ContactsContext.Provider>
+    )
+}
